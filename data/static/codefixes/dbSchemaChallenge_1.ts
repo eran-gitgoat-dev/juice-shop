8329af20f5e9e6159adbeca4c6d77c44a5ae051b
Eran Medan <85855778+eran-medan@users.noreply.github.com>
2022-12-30 12:01:35 -0500

    Update dbSchemaChallenge_1.ts

--- data/static/codefixes/dbSchemaChallenge_1.ts
+++ data/static/codefixes/dbSchemaChallenge_1.ts
@@ -1,7 +1,10 @@
 module.exports = function searchProducts () {
   return (req: Request, res: Response, next: NextFunction) => {
+    // TODO: fix it
     let criteria: any = req.query.q === 'undefined' ? '' : req.query.q ?? ''
+    // TODO: fix it
     criteria = (criteria.length <= 200) ? criteria : criteria.substring(0, 200)
+    // TODO: fix it
     models.sequelize.query("SELECT * FROM Products WHERE ((name LIKE '%"+criteria+"%' OR description LIKE '%"+criteria+"%') AND deletedAt IS NULL) ORDER BY name")
       .then(([products]: any) => {
         const dataString = JSON.stringify(products)
